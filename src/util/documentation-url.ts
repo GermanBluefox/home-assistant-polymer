e2db33789ef7690bcfdcd4069734ebc5266792be
Achim Königs <garfonso@mobo.info>
2020-11-02 11:18:41 +0100

    Update documentation-url.ts

--- src/util/documentation-url.ts
+++ src/util/documentation-url.ts
@@ -1,11 +1,8 @@
 import { HomeAssistant } from "../types";
 
 export const documentationUrl = (hass: HomeAssistant, path: string) => {
-  return `https://${
-    hass.config.version.includes("b")
-      ? "rc"
-      : hass.config.version.includes("dev")
-      ? "next"
-      : "www"
-  }.home-assistant.io${path}`;
+  // IoB Help Link:
+  return `https://www.iobroker.net/#${
+    hass!.language ? hass!.language.split("-")[0] || "en" : "en"
+  }/adapters/adapterref/iobroker.lovelace/README.md`;
 };
